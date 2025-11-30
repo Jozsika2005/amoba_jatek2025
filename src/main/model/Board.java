d73bdaa7b7f127e9fd4a8fba48ce68a7d9cc5872
Jozsika2005 <144223925+Jozsika2005@users.noreply.github.com>
2025-11-30 16:45:42 +0100

    Update Board.java

--- src/main/model/Board.java
+++ src/main/model/Board.java
@@ -1,1 +1,44 @@
+package hu.uni.amoeba.model;
 
+
+public class Board {
+private final Player[][] grid = new Player[3][3];
+
+
+public Board() {
+
+}
+
+
+public Player get(int r, int c) {
+return grid[r][c];
+}
+
+
+public void set(int r, int c, Player p) {
+grid[r][c] = p;
+}
+
+
+public boolean isFull() {
+for (int i = 0; i < 3; i++) {
+for (int j = 0; j < 3; j++) {
+if (grid[i][j] == null) return false;
+}
+}
+return true;
+}
+
+
+public void print() {
+System.out.println("-------------");
+for (int i = 0; i < 3; i++) {
+System.out.print("| ");
+for (int j = 0; j < 3; j++) {
+System.out.print((grid[i][j] == null ? ' ' : grid[i][j].name()) + " | ");
+}
+System.out.println();
+System.out.println("-------------");
+}
+}
+}
