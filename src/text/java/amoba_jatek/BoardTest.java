b1ecbd5dc382c8cd30d9520e0785523c7612008d
Jozsika2005 <144223925+Jozsika2005@users.noreply.github.com>
2025-11-30 16:57:00 +0100

    Update BoardTest.java

--- src/text/java/amoba_jatek/BoardTest.java
+++ src/text/java/amoba_jatek/BoardTest.java
@@ -1,1 +1,16 @@
+package hu.uni.amoeba;
 
+
+import hu.uni.amoeba.model.Board;
+import hu.uni.amoeba.model.Player;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class BoardTest {
+
+
+@Test
+public void testEmptyBoardIsNotFull()
