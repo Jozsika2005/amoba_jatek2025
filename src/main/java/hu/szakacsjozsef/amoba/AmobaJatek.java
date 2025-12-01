598308974e492f07532279a32e0983c621b607ae
Jozsika2005 <144223925+Jozsika2005@users.noreply.github.com>
2025-12-01 10:00:09 +0100

    Update AmobaJatek.java

--- src/main/java/hu/szakacsjozsef/amoba/AmobaJatek.java
+++ src/main/java/hu/szakacsjozsef/amoba/AmobaJatek.java
@@ -6,7 +6,7 @@ public class AmobaJatek {
         Scanner scanner = new Scanner(System.in);
         char[][] tabla = new char[3][3];
 
-        // tábla inicializálása
+    
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 tabla[i][j] = ' ';
