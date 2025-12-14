552c0ced850a45a5e710c4f8100c1d48f32f393e
Jozsika2005 <144223925+Jozsika2005@users.noreply.github.com>
2025-12-14 18:20:35 +0100

    Update BoardTest.java

--- src/main/java/hu/szakacsjozsef/amoba/resources/test/java/hu/szakacsjozsef/amoba/BoardTest.java
+++ src/main/java/hu/szakacsjozsef/amoba/resources/test/java/hu/szakacsjozsef/amoba/BoardTest.java
@@ -1,16 +1,31 @@
-package hu.uni.amoba;
+package com.example.amoba;
 
 
-import hu.uni.amoba.model.Board;
-import hu.uni.amoba.model.Player;
+import com.example.amoba.model.Board;
+import com.example.amoba.model.Player;
+import com.example.amoba.model.Move;
 import org.junit.jupiter.api.Test;
-
-
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class BoardTest {
+@Test
+public void testPlaceAndFull() {
+Board b = new Board(3);
+Player p = new Player("A", 'X');
+assertTrue(b.place(new Move(0,0,p)));
+assertFalse(b.place(new Move(0,0,p))); // already occupied
+}
 
 
 @Test
-public void testEmptyBoardIsNotFull()
+public void testWinnerRow() {
+Player p1 = new Player("A",'X');
+Player p2 = new Player("B",'O');
+Board b = new Board(3);
+b.place(new Move(0,0,p1));
+b.place(new Move(0,1,p1));
+b.place(new Move(0,2,p1));
+assertTrue(b.checkWinner(p1,p2).isPresent());
+}
+}
