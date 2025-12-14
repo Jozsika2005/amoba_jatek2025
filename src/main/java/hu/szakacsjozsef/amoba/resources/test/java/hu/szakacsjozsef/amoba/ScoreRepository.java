777bcbb9fd7a8a66e5617d129df12b5305324e1d
Jozsika2005 <144223925+Jozsika2005@users.noreply.github.com>
2025-12-14 18:16:10 +0100

    Update ScoreRepository.java

--- src/main/java/hu/szakacsjozsef/amoba/resources/test/java/hu/szakacsjozsef/amoba/ScoreRepository.java
+++ src/main/java/hu/szakacsjozsef/amoba/resources/test/java/hu/szakacsjozsef/amoba/ScoreRepository.java
@@ -1,1 +1,13 @@
+package com.example.amoba.persistence;
 
+
+import java.util.List;
+import java.util.Map;
+
+
+public interface ScoreRepository {
+void init() throws Exception;
+void saveScore(String playerName, int score) throws Exception;
+List<Map<String,Object>> topScores(int limit) throws Exception;
+void close() throws Exception;
+}
